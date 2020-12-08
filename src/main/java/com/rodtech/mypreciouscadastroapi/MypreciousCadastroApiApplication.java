5f09d640a239f498d7c4516476bb4849d3c7db1f
Rodrigo Lima <rodrigolimasd@gmail.com>
2020-12-08 18:38:04 -0300

    primeiro commit

---
+++ src/main/java/com/rodtech/mypreciouscadastroapi/MypreciousCadastroApiApplication.java
@@ -0,0 +1,13 @@
+package com.rodtech.mypreciouscadastroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypreciousCadastroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypreciousCadastroApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodtech/mypreciouscadastroapi/MypreciousCadastroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodtech.mypreciouscadastroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypreciousCadastroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
